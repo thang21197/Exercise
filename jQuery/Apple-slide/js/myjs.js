c491ecdaf2a46b0c0aab3a1ec4c9bc2b06b11fc6
thang21197 <48194900+thang21197@users.noreply.github.com>
2020-02-08 00:25:29 +0700

    1

--- jQuery/Apple-slide/js/myjs.js
+++ jQuery/Apple-slide/js/myjs.js
@@ -3,10 +3,12 @@ jQuery(document).ready(function($) {
 	    console.log($('.box-slider .slider-img').length);
         $('.button-right').click(function(event) {
         current_slider=$('.box-slider .slider-img.active');
+
         $('.box-slider .slider-img.active').removeClass('active');
     	current_slider.next().addClass('active');
+    	
         if (current_slider==$('.box-slider .slider-img').length()){
-        	current_slider=0;
+           
         }
      });
 });
