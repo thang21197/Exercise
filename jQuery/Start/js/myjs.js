6e05bb16f6655dcc7d05ea7d6778586463da1f04
thang21197 <48194900+thang21197@users.noreply.github.com>
2020-02-07 17:52:45 +0700

    1

--- jQuery/Start/js/myjs.js
+++ jQuery/Start/js/myjs.js
@@ -1,4 +1,11 @@
 jQuery(document).ready(function($) {
+	$('.Our-work .extra-img').slideUp();
+	$('.Our-work .btn-dark.btn').click(function(event) {
+		event.preventDefault();
+		$('.Our-work .extra-img').slideToggle();
+		$('html,body').animate({scrollTop:$('.Our-work .extra-img').offset().top});
+		TweenMax.staggerFrom($('.extra-img .col-md-6'),0.8,{top:200,opacity:0},0.2);
+	});
 	$('.btn-close').click(function(event) {
 		event.preventDefault();
 		$('.menu-side-bar').toggleClass('active');
@@ -9,6 +16,23 @@ jQuery(document).ready(function($) {
 		$('.menu-side-bar').toggleClass('active');
 	});
 	$('a.home').click(function(event) {
-		$(window).animate({scrollTop:0}, 1000);
+		event.preventDefault();
+		$('html,body').animate({scrollTop:0}, 1000,"easeInOutSine");
+	});
+	$('a.about').click(function(event) {
+		event.preventDefault();
+		$('html,body').animate({scrollTop:$('.text-after-banner').offset().top}, 1000,"easeInOutSine");
+	});
+	$('a.services').click(function(event) {
+		event.preventDefault();
+		$('html,body').animate({scrollTop:$('.service').offset().top}, 1000,"easeInOutSine");
+	});
+	$('a.portfolio').click(function(event) {
+		event.preventDefault();
+		$('html,body').animate({scrollTop:$('.section-2').offset().top}, 1000,"easeInOutSine");
+	});
+	$('a.contact').click(function(event) {
+		event.preventDefault();
+		$('html,body').animate({scrollTop:$('.section-map').offset().top}, 1000,"easeInOutSine");
 	});
 });
